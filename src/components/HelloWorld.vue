5953ee9a46dfefc9b7b391dac5634349806f4041
Brandon Whisnant <whisnantb@locustec.com>
2018-12-12 15:53:30 -0500

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,70 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+    <p>{{ msg }} {{ name }}</p>
+    <h3>Hobbies</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li v-for="(interest,index) in interests" :key="index">{{ interest }}</li>
     </ul>
-    <h3>Essential Links</h3>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+
+      <li v-for="(stat, index) in stats" :key="index">{{ index }}: {{ stat }}</li>
     </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+    <button v-on:click="increment">+</button>
+    <p>{{ count }}</p>
+    <button v-on:click="decrement">-</button>
+    <br>
+    <br>
+    <form action="" @keydown.enter.prevent.self>
+      <input type="text" v-on:keyup.enter="addInputValue" v-on:keyup.backspace="count = 0">
+    </form>
+    
+
+   
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  data(){
+    return {
+      msg: 'Hello',
+      name: 'Brandon',
+      interests: [
+        'programming', 'napping', 'petting gregg'
+      ],
+      stats: {
+        Name: 'Brandon Whisnant',
+        Profession: 'Software Developer',
+        Age: 26
+      },
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count += 1
+     
+    },
+    decrement() {
+      this.count -= 2
+    },
+    addInputValue(event) {
+      this.value = event.target.value
+      this.count += this.value
+      this.count = parseInt(this.count)
+    }
+  }  
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  li {
+    list-style: none;
+    padding: 10px;
+    display: inline-block;
+  }
+
 </style>
